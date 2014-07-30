10a7d8edbfb1f9eb36941df48662d68fe5a4cb64
Will Faurot <wfaurot@gmail.com>
2014-07-30 02:35:34 -0700

    Refactor to move logic to models

--- lib/app/models/location.rb
+++ lib/app/models/location.rb
@@ -1,2 +1,23 @@
-class Location < Sequel::Model(:locations)
+require_relative '../../database'
+
+class Location < Sequel::Model(:location)
+
+  def initialize(id)
+    @id = id
+  end
+
+  def hash_data
+    DB[:location].where(:id => @id.to_i).to_a.first
+  end
+
+  def update_location(params)
+    location = hash_data
+    location[:address]       = params[:address]
+    location[:email_address] = params[:email_address]
+    location[:phone_number]  = params[:phone_number]
+    location[:description]   = params[:description]
+
+    DB[:location].where(:id => @id.to_i).update(location)
+  end
+
 end
