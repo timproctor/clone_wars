d523f1f3fa811522affd2b91d4da822b907a6e51
Will Faurot <wfaurot@gmail.com>
2014-07-30 09:21:26 -0600

    Add login tests to main admin test file

--- test/features/admin_user_test.rb
+++ test/features/admin_user_test.rb
@@ -2,6 +2,31 @@ require_relative 'feature_test_helper'
 
 class AdminUserTest < FeatureTest
 
+  def test_user_fails_to_log_in_as_admin
+    visit '/'
+    assert_equal 200, page.status_code
+
+    click_link('Login Here')
+    assert_equal 200, page.status_code
+
+    fill_in('username', with: 'ned')
+    fill_in('password', with: 'stark')
+    click_button('Login')
+
+    assert page.has_content?("Login failed")
+  end
+
+  def test_user_successfully_logs_in_as_admin
+    visit '/login'
+    assert_equal 200, page.status_code
+
+    fill_in('username', with: 'jon')
+    fill_in('password', with: 'snow')
+    click_button('Login')
+
+    assert page.has_css?('.admin-header')
+  end
+
   def login_as_admin
     visit '/login'
     fill_in('username', with: 'jon')
