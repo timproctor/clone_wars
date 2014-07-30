a0343860ccfb0e414407d6b89837ba097f204285
Will Faurot <wfaurot@gmail.com>
2014-07-29 18:51:05 -0600

    Add test for viewing admin console

--- test/features/admin_user_test.rb
+++ test/features/admin_user_test.rb
@@ -1,16 +1,18 @@
 require_relative 'feature_test_helper'
 
 class AdminUserTest < FeatureTest
-  describe 'Admin User' do
-      it 'can see a page on admin dashboard' 
-      it 'can select location on admin dashboard'
-      it 'can select menu on admin dashboard'
-      it 'can see location editor page'
-      it 'can edit a location'
-      it 'can see menu editor page'
-      it 'can add a menu item'
-      it 'can delete a menu item'
-      it 'can update a menu item'
+
+  def test_admin_can_visit_the_admin_console
+    visit '/login/admin_dashboard'
+    assert_equal 200, page.status_code
   end
+    # it 'can select location on admin dashboard'
+    # it 'can select menu on admin dashboard'
+    # it 'can see location editor page'
+    # it 'can edit a location'
+    # it 'can see menu editor page'
+    # it 'can add a menu item'
+    # it 'can delete a menu item'
+    # it 'can update a menu item'
 
 end
